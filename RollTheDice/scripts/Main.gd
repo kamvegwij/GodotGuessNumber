8df6e4524aa1d53f17fbd2131d7438ec73840ba2
Kamve Gwijana <gwijgwij011@gmail.com>
2023-01-13 22:37:16 +0200

    android deployment setup

--- RollTheDice/scripts/Main.gd
+++ RollTheDice/scripts/Main.gd
@@ -1,22 +1,29 @@
 extends Control
 
+export (NodePath) onready var loadTimer = get_node(loadTimer)
 export (NodePath) onready var numText = get_node(numText)
 export (NodePath) onready var inputEdit = get_node(inputEdit)
 
 var guess = ""
-var numberToGuess = 5 #initially set the guess to 3
+var numberToGuess = 1 #initially set the guess to 3
 var timesFailed = 0
 var randGen = RandomNumberGenerator.new()
 var randNum = 0
+
+var loadDone: bool = false
 
 func _on_roll_button_down():
 	rollTheDice()	
 	guess()
 	
 func rollTheDice():
+	loadTimer.start(5) #TAKES 5 SECONDS TO SHOW THE GENERATED NUM
 	randGen.randomize()
 	randNum = randGen.randi_range(0,numberToGuess)
-	numText.text = str(randNum)
+	
+	if loadDone:
+		#SHOW AFTER TIMER IS DONE
+		numText.text = str(randNum)
 	
 func guess():
 	guess = inputEdit.text
@@ -37,7 +44,6 @@ func fail():
 		
 func loadFailScene():
 	get_tree().change_scene("res://scenes/Menu.tscn")
-	
 
-
-
+func _on_loadTimer_timeout():
+	loadDone = true
